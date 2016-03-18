4ad5ae41ddb82b008db69ae2dc1a560be18e1780
Patrik Ackland <patrikackland@gmail.com>
2016-03-18 00:27:36 -0700

    Almost done, need server for streak

--- card.js
+++ card.js
@@ -17,38 +17,69 @@ $(function() {
     }
 
     function draw_card(data) {
-        var card = $("#github-card");
-        card.width(300).height(100);
-        card.css("font-family", "Helvetica, arial, nimbussansl, liberationsans, freesans, clean, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'");
-        card.css("position", "absolute");
-        card.css("border", "solid 1px #D3D3D3");
-        card.css("border-radius", "3px");
-        card.css("padding", 10);
-        var img = $("<img />", {
-            src: data['avatar_url']
-        });
-        img.css("max-width", 102);
-        img.css("max-height", 102);
-        card.html(img);
-        var name = $("<h2>");
-        name.css("position", "absolute");
-        name.html(data['name']);
-        name.css("color", "rgb(51,51,51)");
-        name.css("margin", 0);
-        name.css("top", 10);
-        name.css("left", 122);
+        var card = $("#github-card")
+            .width(300).height(100)
+            .css("font-family", "Helvetica, arial, nimbussansl, liberationsans, freesans, clean, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'")
+            .css("position", "absolute")
+            .css("border", "solid 1px #D3D3D3")
+            .css("border-radius", "3px")
+            .css("padding", 10);
+        var img = $("<img />", {src: data['avatar_url']})
+            .css("max-width", 102)
+            .css("max-height", 102);
+        var name = $("<h2>")
+            .css("position", "absolute")
+            .html(data['name'])
+            .css("color", "rgb(51,51,51)")
+            .css("margin", 0)
+            .css("top", 10)
+            .css("left", 122);
+        var uname = $("<p>")
+            .css("position", "absolute")
+            .html(data['login'])
+            .css("top", 38)
+            .css("left", 122)
+            .css("margin",0)
+            .css("font-size","14px")
+            .css("color", "#666");
         card.append(name);
-        var repos = $("<h3>");
-        repos.html(data['public_repos']);
-        repos.css("position", "absolute");
-        repos.css("color", "rgb(64,120,192)");
-        repos.css("bottom", 10);
-        repos.css("margin", 0);
-        repos.css("left", 122);
-        card.append(repos);
-        var repoText = $("<p>");
-        repoText.html("Repositories");
-        repoText.css("color", "rgb(118,118,118");
-        card.append(repoText);
+        var repo = create_stats_div(
+                data['public_repos'],
+                "Repositories")
+            .css("left", 122);
+        var streak = create_stats_div("107","Streak")
+            .css("left", 186);
+        var followers = create_stats_div(
+                data['followers'],
+                "Followers")
+            .css("left", 250);
+        card.html(img)
+            .append(name)
+            .append(uname)
+            .append(repo)
+            .append(streak)
+            .append(followers);
+    }
+
+    function create_stats_div(n, s) {
+        var container = $("<div>")
+            .css("top",65)
+            .width(64)
+            .css("position","absolute");
+        var num = $("<h3>")
+            .html(n)
+            .css("color", "rgb(64,120,192)")
+            .css("text-align", "center")
+            .css("margin", "auto 0");
+        var txt = $("<p>")
+            .html(s)
+            .css("margin",0)
+            .css("color", "rgb(118,118,118")
+            .css("text-align", "center")
+            .css("font-size", "10px");
+        container
+            .append(num)
+            .append(txt);
+        return container;
     }
 });
