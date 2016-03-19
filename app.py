c8c5104ed375dee2b7412558986e53e96607c0e9
Patrik Ackland <patrikackland@gmail.com>
2016-03-18 22:02:53 -0700

    No debug

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import os
 
 app = Flask(__name__)
 
@@ -8,5 +7,4 @@ def github_streak():
     return request.data
 
 if __name__ == "__main__":
-    app.debug=True
     app.run()
