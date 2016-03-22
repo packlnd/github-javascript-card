bb6af281f52bfed71b25e19315d7bbaa0f37af38
Patrik Ackland <patrikackland@gmail.com>
2016-03-21 18:07:44 -0700

    Stringify etc

--- twitter.js
+++ twitter.js
@@ -5,7 +5,8 @@ $(function() {
         url: "http://localhost:5000/twitter",
         data: {'screen_name': sname},
         success: function(data) {
-            $("#twitter-card").html(data);
+            console.log(data);
+            $("#twitter-card").html(JSON.stringify(data));
         }
     });
 });
